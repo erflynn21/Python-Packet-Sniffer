1d89c6904fdcbf59176e1bb3df853faff6f2a6cb
Evan <erflynn21@gmail.com>
2020-05-08 03:12:26 -0400

    Results are filtered to pull out potential usernames and passwords

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -10,7 +10,8 @@ def sniff(interface):
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        print(packet)
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
 
 
 sniff("eth0")
